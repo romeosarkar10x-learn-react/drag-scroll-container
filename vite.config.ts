0124e1198772646527259e6570e9d43482b2bf09
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-02-19 22:16:48 +0530

    Added tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 function getBaseURL(): string {
     const githubRepository = process.env.GITHUB_REPOSITORY;
@@ -14,7 +15,7 @@ function getBaseURL(): string {
 }
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     base: getBaseURL(),
 
     resolve: {
